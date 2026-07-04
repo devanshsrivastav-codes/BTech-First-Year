a4af8a3b03f11348ca2935976cad09705710103b
Devansh Srivastava <devanshjpmschool@gmail.com>
2026-07-04 20:57:08 +0530

    Create ch2 problem solving(self).c

---
+++ chapter 2/ch2 problem solving(self).c
@@ -0,0 +1,64 @@
+// in this code i will solve some questions which is writen by me or chatgpt
+// Instructions:
+// 1. This file contains multiple C programs.
+// 2. To run a program, uncomment only that program.
+// 3. Keep all other programs commented.
+// 4. Save the file before running.
+
+
+//program number 1
+// Question
+
+// Write a C program that:
+
+// Takes two integers as input from the user.
+// Prints only their sum.
+
+// #include <stdio.h>
+
+// int main(){
+//     int first_number;
+//     printf("enter the first number");
+//     scanf("%d", &first_number);
+//     int second_number;
+//     printf("enter the second number");
+//     scanf("%d", &second_number);
+//     printf("the sum of two numbers %d and %d is : %d", first_number, second_number, first_number + second_number); // Number of %d and variables in printf() must be the same.
+//     return 0;
+    
+// }
+
+// after a single mistake this program is executed successfully
+//and the 1st program is between line 17-29
+
+
+
+//program number 2
+// Question
+
+// Write a C program that:
+
+// Takes two integers as input from the user.
+// Prints:
+// Sum
+// Difference
+// Product
+
+// #include <stdio.h>
+
+// int main(){
+//     int first_number;
+//     printf("enter the first number(only integer) : ");
+//     scanf("%d", &first_number);
+//     int second_number;
+//     printf("enter the second number (only integer) : ");
+//     scanf("%d", &second_number);
+//     printf("the sum of %d and %d is : %d\n", first_number, second_number, first_number + second_number);
+//     printf("the difference of %d and %d is : %d\n", first_number, second_number, first_number - second_number);
+//     printf("the product of %d and %d is : %d\n", first_number, second_number, first_number * second_number);
+
+//     return 0;
+// }
+
+// without any mistake this program is executed successfully
+//and the 2nd program is between line 47-61
