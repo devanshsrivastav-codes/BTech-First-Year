819e83b2c28f821a65c04a960a150baa26267e70
Devansh Srivastava <devanshjpmschool@gmail.com>
2026-07-06 21:41:21 +0530

    Create ch3.1 if else.c

---
+++ chapter 3/ch3.1 if else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int age = 5;
+    if(age>10){
+        printf("Your age is greater then 10 \n");
+
+    }
+    else{
+        printf("your age is less then 10 \n");
+        
+    }
+
+    return 0;
+}
